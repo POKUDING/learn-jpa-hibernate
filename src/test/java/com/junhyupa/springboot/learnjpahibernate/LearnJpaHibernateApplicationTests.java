7c5de81b7cca2f7dd4c73301bc9cf0b9a65e9919
jun <yoseb95@gmail.com>
2024-03-28 14:58:52 +0900

    first commit

---
+++ src/test/java/com/junhyupa/springboot/learnjpahibernate/LearnJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhyupa.springboot.learnjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
